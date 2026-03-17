e5f9f7838c6d9c810ab6cb7bafc9869f6394bca3
MuradAydi <muradaydi06@gmail.com>
2026-03-17 17:57:38 +0200

    testing tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,37 +5,5 @@ export default function App() {
                 Tailwind is working
             </h1>
         </div>
-<<<<<<< HEAD
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit.. <code>src/App.jsx</code> and save to tests <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-
-        {}
-        <div className="backend-data">
-          {data ? <p>Backend says: {data}</p> : <p>Loading backend data...</p>}
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        {}
-      </section>
-    </>
-  )
-}
-
-export default App
-=======
     )
 }
->>>>>>> 04a18f160f24fadfbab574b4894e3494289b0369
